7f9ae78207553b37637cf268d7b02adb02c45b9a
rocco de luca <roccodeluca865@gmail.com>
2020-06-08 16:35:12 +0200

    php-oop-dipendenti

---
+++ classes/Dipendenti.php
@@ -0,0 +1,43 @@
+<?php
+
+class Employee {
+    public $fresher;
+    public $name;
+    public $surname;
+    public $birthPlace;
+    public $dateOfBirth;
+    public $residance;
+    public function __construct($_fresher, $_name, $_surname, $_birthPlace, $_dateOfBirth, $_residance){
+        $this->fresher = $_fresher;
+        $this->name = $_name;
+        $this->surname = $_surname;
+        $this->birthPlace = $_birthPlace;
+        $this->dateOfBirth = $_dateOfBirth;
+        $this->residance = $_residance;
+    }
+public function visualizza_Dipendenti()
+{
+    $result = ['Name'=>$this->name,]
+}
+}
+
+class Worker extends 
+{
+    public $notturno;
+    function __construct($_matricola, $_nome, $_cognome, $_luogoNascita, $_dataNascita, $_residenza, $_notturno){
+        parent::__construct($_matricola, $_nome, $_cognome, $_luogoNascita, $_dataNascita, $_residenza);
+        $this->notturno = $_notturno;
+    }
+}
+
+class Manager extends Employee{
+    public $num;
+    public function setNum($num) {
+        if(! is_int($num)){
+            throw new Exception('is not a number');
+        }
+        else{
+            $this->num = $_num;
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once __DIR__ . '/classes/Employees.php';
+
+$director = new Manager();
+
+var_dump($director);
+
+try {
+    $director->setNum(5)
+}
+catch (Exception $e) {
+    echo 'error' . $e->getMessage();
+}
